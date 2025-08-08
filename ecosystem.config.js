73d6de7fb7a37f9b7cefa7bd9d68f2c070311eda
root <root@ubuntu-cloudpanel.localhost>
2025-08-08 06:28:40 +0000

    added ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'foodfly-nextjs',
-      script: './start-standalone.sh',
+      script: 'npm',
+      args: 'start',
       cwd: '.',
       env: {
         NODE_ENV: 'production',
